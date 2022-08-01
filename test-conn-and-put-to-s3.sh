0d4267dc71ea364cbb8a375f8ca71b8d4759dbf6
root <root@ip-10-93-42-202.ap-southeast-3.compute.internal>
2022-08-01 13:37:33 +0000

    parameterize s3 bucket name and path inside the bucket

--- test-conn-and-put-to-s3.sh
+++ test-conn-and-put-to-s3.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 output_folder=/tmp
 output_file=$(TZ='Asia/Jakarta' date +%Y%m%d%H%M%S)-conn-test-result.txt
-s3_bucket=blah
-s3_path=anu
+s3_bucket=$(echo -e -n $(aws ssm get-parameter --name /conntest/bucketname | jq -r .Parameter.Value))
+s3_path=$(echo -e -n $(aws ssm get-parameter --name /conntest/bucketpath | jq -r .Parameter.Value))
 
-/root/test-conn.sh | tee $output_folder/$output_file
-#aws s3 cp $output_folder/$output_file s3://$s3_bucket/$s3_path
+./test-conn.sh | tee $output_folder/$output_file
+aws s3 cp $output_folder/$output_file s3://$s3_bucket/$s3_path
